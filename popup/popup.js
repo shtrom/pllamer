7e6fef203d2cb05301056d030e9bf603d8d35bfe
Olivier Mehani <omehani@mozilla.com>
2024-12-06 16:51:01 +1100

    Some debugging

--- popup/popup.js
+++ popup/popup.js
@@ -13,10 +13,33 @@ document.getElementById("actionButton").addEventListener("click", () => {
 });
 
 async function plan(objective) {
-  await browser.trial.ml.createEngine({taskName: "text-generation"});
-  const res = await browser.trial.ml.runEngine({
-    args: [prompt + objective]
-  });
+  //await browser.trial.ml.createEngine({taskName: "text-generation"});
+  //
+  //const res = await browser.trial.ml.runEngine({
+  //  args: [prompt + objective]
+  //});
+  //
+  //console.log(res);
   
-  console.log(res);
+  // 1. Initialize the event listener
+  browser.trial.ml.onProgress.addListener(progressData => {
+    console.log(progressData);
+  });
+
+  // 2. Create the engine, may trigger downloads.
+  await browser.trial.ml.createEngine({
+    modelHub: "huggingface",
+    taskName: "summarization",
+  });
+
+  // 3. Call the engine
+  const res = await browser.trial.ml.runEngine({
+    args: ["This is the text to summarize"],
+  });
+
+  // 4. Get the results.
+  console.log(res[0]["summary_text"]);
+
+  // 5. Delete the downloaded model files
+  await browser.trial.ml.deleteCachedModels();
 }
