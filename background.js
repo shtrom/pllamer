d2d32e605f26a5f5267ecba7d4367a984fc888f9
Olivier Mehani <omehani@mozilla.com>
2024-12-06 15:45:15 +1100

    First draft
    References:
    * https://firefox-source-docs.mozilla.org/toolkit/components/ml/extensions-api-example/README.html#trial-inference-api-extension-example
    * https://firefox-source-docs.mozilla.org/toolkit/components/ml/extensions.html
      * https://searchfox.org/mozilla-central/source/toolkit/components/ml/docs/extensions-api-example/

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed.");
+  console.log(browser);
+  console.log(browser.trial);
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,22 @@
+//const { createEngine } = ChromeUtils.importESModule("chrome://global/content/ml/EngineProcess.sys.mjs");
+
+const prompt = "Could you create a todo list including pre-filled links to relevant web-searches, as well as criteria to evaluate the results of those searches? The objective is: ";
+
 document.getElementById("actionButton").addEventListener("click", () => {
-  alert("Button clicked!");
+
+  const objective = document.getElementById("objectiveInput").value;
+  alert("Button clicked!" + objective);
+  alert(browser.trial.ml);
+
+  plan(objective);
+
 });
+
+async function plan(objective) {
+  await browser.trial.ml.createEngine({taskName: "text-generation"});
+  const res = await browser.trial.ml.runEngine({
+    args: [prompt + objective]
+  });
+  
+  console.log(res);
+}
